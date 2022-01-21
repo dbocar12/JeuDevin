b88ea0fda1322af5d979b51a1375e430cc45bbfe
dbocar12 <33195239+dbocar12@users.noreply.github.com>
2022-01-21 15:28:02 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'JeuDevin';
+  deviation!: number;
+  noOfTries!: number;
+  original!: number;
+  guess!: number;
+  noOfTriesRestant!: number;
+  buttonDisabled!: boolean;
+
+  constructor() {
+    this.initialiserJeu();
+  }
+  initialiserJeu() {
+    this.noOfTries = 0;
+    this.original = Math.floor((Math.random() * 100) + 1);
+    console.log(this.original);
+    this.guess = 0;
+    this.deviation = 0;
+    this.noOfTriesRestant = 5;
+    this.buttonDisabled = false;
+  }
+  verifierDevin() {
+    this.deviation = this.original - this.guess;
+    this.noOfTries = this.noOfTries + 1;
+    this.noOfTriesRestant = 5 - this.noOfTries;
+    if(this.noOfTriesRestant == 0 || this.deviation == 0) {
+      this.buttonDisabled = true;
+    }
+  }
 }
